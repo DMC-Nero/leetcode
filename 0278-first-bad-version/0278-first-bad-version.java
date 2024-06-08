c8844edb843a1a9dffb3e8e61be711437935f28f
DMC-Nero <155235476+DMC-Nero@users.noreply.github.com>
2024-06-08 17:56:33 +0530

    Time: 24 ms (98.22%), Space: 40.2 MB (40.30%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,20 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int begin = 1;
+        int end = n;
+        while(begin < end) {
+            int mid = begin + (end - begin) /2;
+            boolean isBad = isBadVersion(mid);
+
+            if(isBad == true) {
+                end = mid;
+            }else {
+                begin = mid + 1;
+            }
+        }
+        return begin;
+    }
+}
