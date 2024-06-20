1e75a870e1ec06695c7c32099d8df4088bb685da
DMC-Nero <155235476+DMC-Nero@users.noreply.github.com>
2024-06-20 22:38:24 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (65.11%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int[] count = new int[102];
+        
+        for(int num : nums) {
+            count[num]++;
+        }
+        int totalcount = 0;
+        
+        for(int i : count) {
+            totalcount += ((i) * (i-1))/2;
+        }
+        
+        return totalcount;
+    }
+}
